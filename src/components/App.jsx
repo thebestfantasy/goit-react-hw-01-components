a8dc877e516e76d8a4e6f9b2e3dc3ce521235cf3
thebestfantasy <sergej.fronch@gmail.com>
2023-07-03 23:00:09 +0300

    sd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,36 @@
+import React from "react"
+import { Profile } from "./Profile/Profile.js"
+import user from "./Profile/user.json"
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}/>
     </div>
-  );
-};
+  )
+}
+
+
+
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
